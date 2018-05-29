5cb488262daf0bc3ae8085f45e3152e69e9ff1b6
Sushant <sushant6309@gmail.com>
2018-05-29 23:29:56 +0530

    Feat: Oauth in progress

--- services/passport.js
+++ services/passport.js
@@ -7,13 +7,14 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys');
 const User = mongoose.model('users');
 
-passport.serializeUser((user, done)=>{
-    done(null, user.id);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then(user => done(null, user));
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 
